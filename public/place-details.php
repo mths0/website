c79636b41321cc8cf0cbfa5c1dde3459c0362f71
majed <work+git@majed7d3.com>
2026-05-09 17:20:31 +0300

    change to arabic

--- public/place-details.php
+++ public/place-details.php
@@ -85,13 +85,13 @@
                     </article>
                     <?php
                 } else {
-                    echo "Place not found.";
+                    echo "لم نجد المكان.";
                 }
             } else {
-                echo "Error fetching place details.";
+                echo "خطأ في استدعاء معلومات المكان.";
             }
         } else {
-            echo "No place ID specified.";
+            echo "لا يوجد مكان برقم التعريفي هذا.";
         }
         ?>
     </main>
